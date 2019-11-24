aad5f90c07be4c357ccdda70df11f8fbc7ab1212
Igor Matuszewski <xanewok@gmail.com>
2019-11-24 21:22:01 +0100

    Implement pretty-printing

---
+++ src/__tests__/printer.test.ts
@@ -0,0 +1,141 @@
+import parseFilter, {
+  Operator,
+  parseCondition,
+  Condition,
+  Primitive,
+  Range,
+  parseRange,
+  LogicalOperator,
+  Filter
+} from "../parser";
+import {
+  printOperator,
+  printPrimitive,
+  printCondition,
+  printRangeOfPrimitives,
+  printLogicalOperator,
+  printFilter
+} from "../printer";
+
+test("Primitives", () => {
+  expect(printPrimitive(123)).toBe("123");
+  expect(printPrimitive(-123)).toBe("-123");
+  expect(printPrimitive(0.511)).toBe("0.511");
+  expect(printPrimitive(" a string ")).toBe(" a string ");
+});
+
+test("Operators", () => {
+  const cases: { [k: string]: Operator } = Operator;
+  for (const testCase in cases) {
+    const [text, value] = [testCase, cases[testCase]];
+
+    expect(printOperator(value)).toBe(text);
+  }
+});
+
+test("Logical operators", () => {
+  const cases: { [k: string]: LogicalOperator } = LogicalOperator;
+
+  for (const testCase in cases) {
+    const [text, value] = [testCase, cases[testCase]];
+
+    expect(printLogicalOperator(value)).toBe(text);
+  }
+});
+
+test("Conditions", () => {
+  const cases: { [k: string]: Condition } = {
+    "eq.20": { negated: false, operator: Operator.eq, value: 20 },
+    "not.eq.20": { negated: true, operator: Operator.eq, value: 20 },
+    "hi.not.eq.20": {
+      ident: "hi",
+      negated: true,
+      operator: Operator.eq,
+      value: 20
+    },
+    "cs.{example,new}": {
+      negated: false,
+      operator: Operator.cs,
+      value: ["example", "new"]
+    },
+    "in.[1,2]": {
+      negated: false,
+      operator: Operator.in,
+      value: {
+        left: { kind: "including", value: 1 },
+        right: { kind: "including", value: 2 }
+      }
+    }
+  };
+
+  for (const testCase in cases) {
+    const [text, value] = [testCase, cases[testCase]];
+
+    expect(printCondition(value)).toBe(text);
+    expect(parseCondition.parse(text)).toMatchObject({ value });
+  }
+});
+
+test("Ranges", () => {
+  const cases: { [k: string]: Range<Primitive> } = {
+    "[1,2]": {
+      left: { kind: "including", value: 1 },
+      right: { kind: "including", value: 2 }
+    },
+    "(1,2]": {
+      left: { kind: "excluding", value: 1 },
+      right: { kind: "including", value: 2 }
+    },
+    "(1,2)": {
+      left: { kind: "excluding", value: 1 },
+      right: { kind: "excluding", value: 2 }
+    }
+    // TODO:
+    // "[2018-05-15,2019-06-01)": {
+    //   left: { kind: "including", value: "2018-05-15" },
+    //   right: { kind: "excluding", value: "2019-06-01" }
+    // }
+  };
+
+  for (const testCase in cases) {
+    const [text, value] = [testCase, cases[testCase]];
+
+    expect(printRangeOfPrimitives(value)).toBe(text);
+    expect(parseRange.parse(text)).toMatchObject({ value });
+  }
+});
+
+test("Filters", () => {
+  const cases: { [k: string]: Filter } = {
+    "or(a.gte.100,a.lte.50)": {
+      negated: false,
+      operator: LogicalOperator.or,
+      value: [
+        { negated: false, ident: "a", operator: Operator.gte, value: 100 },
+        { negated: false, ident: "a", operator: Operator.lte, value: 50 }
+      ]
+    },
+    "and(or(a.gte.1,a.lte.2),not.lte.3)": {
+      negated: false,
+      operator: LogicalOperator.and,
+      value: [
+        {
+          negated: false,
+          operator: LogicalOperator.or,
+          value: [
+            { negated: false, ident: "a", operator: Operator.gte, value: 1 },
+            { negated: false, ident: "a", operator: Operator.lte, value: 2 }
+          ]
+        },
+        { negated: true, operator: Operator.lte, value: 3 }
+      ]
+    }
+  };
+
+  for (const testCase in cases) {
+    const [text, value] = [testCase, cases[testCase]];
+
+    expect(printFilter(value)).toBe(text);
+    expect(parseFilter.parse(text)).toMatchObject({ value });
+  }
+});
---
+++ src/printer.ts
@@ -0,0 +1,77 @@
+import {
+  Condition,
+  Filter,
+  LogicalOperator,
+  Operand,
+  Operator,
+  Range,
+  Primitive,
+  LogicalCondition
+} from "./parser";
+
+export function printPrimitive(val: Primitive): string {
+  return val.toString();
+}
+
+export function printOperator(val: Operator): string {
+  return val.toString();
+}
+
+export function printLogicalOperator(val: LogicalOperator): string {
+  return val.toString();
+}
+
+function printArrayOfPrimitives(val: Primitive[]): string {
+  // Normalize to curly braces
+  return `{${val.map(printPrimitive).join(",")}}`;
+}
+
+export function printOperand(val: Operand): string {
+  return val instanceof Array
+    ? printArrayOfPrimitives(val)
+    : typeof val === "string" || typeof val === "number"
+    ? printPrimitive(val)
+    : printRangeOfPrimitives(val);
+}
+
+export function printCondition(val: Condition): string {
+  const { ident, negated, operator, value } = val;
+  return [ident, negated ? "not" : undefined, operator, printOperand(value)]
+    .filter(x => x !== undefined)
+    .join(".");
+}
+
+export function printRangeOfPrimitives(val: Range<Primitive>): string {
+  const left =
+    val.left.kind == "including"
+      ? `[${printPrimitive(val.left.value)}`
+      : `(${printPrimitive(val.left.value)}`;
+  const right =
+    val.right.kind == "including"
+      ? `${printPrimitive(val.right.value)}]`
+      : `${printPrimitive(val.right.value)})`;
+  return [left, right].join(",");
+}
+
+export function printFilter(val: Filter): string {
+  function isLogicalCondition(cond: Filter): cond is LogicalCondition {
+    return cond.operator in LogicalOperator;
+  }
+
+  return isLogicalCondition(val)
+    ? printLogicalCondition(val)
+    : printCondition(val);
+}
+
+function printFilters(val: Filter[]): string {
+  return `(${val.map(printFilter).join(",")})`;
+}
+
+export function printLogicalCondition(val: LogicalCondition): string {
+  const { negated, operator, value } = val;
+  const printedOperator = [negated ? "not" : undefined, operator]
+    .filter(x => x !== undefined)
+    .join(".");
+
+  return printedOperator + printFilters(value);
+}
